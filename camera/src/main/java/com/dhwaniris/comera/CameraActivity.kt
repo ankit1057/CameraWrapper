7d038d82a5c988be35883c4831ae98cb37a1813e
Anish Bajpai <anishbajpai014@gmail.com>
2018-08-16 08:39:12 +0530

    integrate flash and camera switch

--- camera/src/main/java/com/dhwaniris/comera/CameraActivity.kt
+++ camera/src/main/java/com/dhwaniris/comera/CameraActivity.kt
@@ -5,28 +5,36 @@ import android.net.Uri
 import android.os.Build.VERSION
 import android.os.Build.VERSION_CODES
 import android.os.Bundle
-import android.os.Environment
 import android.provider.MediaStore
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
+import com.dhwaniris.comera.widgets.CameraSwitchView
+import com.dhwaniris.comera.widgets.FlashSwitchView
 import io.fotoapparat.Fotoapparat
+import io.fotoapparat.configuration.CameraConfiguration
 import io.fotoapparat.parameter.Resolution
 import io.fotoapparat.result.BitmapPhoto
-import io.fotoapparat.result.transformer.scaled
+import io.fotoapparat.selector.autoFlash
+import io.fotoapparat.selector.back
+import io.fotoapparat.selector.front
+import io.fotoapparat.selector.off
+import io.fotoapparat.selector.on
 import io.fotoapparat.view.CameraView
-import java.io.File
-import java.text.SimpleDateFormat
-import java.util.Date
 
 
 class CameraActivity : AppCompatActivity() {
 
   private val cameraView by lazy { findViewById<CameraView>(R.id.cameraView) }
   private val capture by lazy { findViewById<View>(R.id.capture) }
+  private val cameraSwitchView by lazy { findViewById<CameraSwitchView>(R.id.cameraSwitchView) }
+  private val flashSwitchView by lazy { findViewById<FlashSwitchView>(R.id.flashSwitchView) }
+
   private lateinit var fotoapparat: Fotoapparat
   private val permissionsDelegate = PermissionsDelegate(this)
   private lateinit var imageUri: Uri
+  private var isBackCamera = true
+  private val flashManager = FlashManager()
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -79,6 +87,38 @@ class CameraActivity : AppCompatActivity() {
             }
           }
     }
+
+    cameraSwitchView.setOnClickListener {
+
+      if (isBackCamera) {
+        fotoapparat.switchTo(front(), CameraConfiguration.default())
+        cameraSwitchView.displayFrontCamera()
+      } else {
+        fotoapparat.switchTo(back(), CameraConfiguration.default())
+        cameraSwitchView.displayBackCamera()
+      }
+
+      isBackCamera = !isBackCamera
+    }
+
+    flashSwitchView.setOnClickListener {
+      val current = flashManager.switch()
+      when (current) {
+        is CameraFlashAuto -> {
+          fotoapparat.updateConfiguration(
+              CameraConfiguration.default().copy(flashMode = autoFlash()))
+          flashSwitchView.displayFlashAuto()
+        }
+        is CameraFlashOn -> {
+          fotoapparat.updateConfiguration(CameraConfiguration.default().copy(flashMode = on()))
+          flashSwitchView.displayFlashOn()
+        }
+        is CameraFlashOff -> {
+          fotoapparat.updateConfiguration(CameraConfiguration.default().copy(flashMode = off()))
+          flashSwitchView.displayFlashOff()
+        }
+      }
+    }
   }
 
   override fun onStart() {
@@ -102,5 +142,25 @@ class CameraActivity : AppCompatActivity() {
       fotoapparat.start()
       cameraView.visibility = View.VISIBLE
     }
+  }
+}
+
+sealed class CameraFlashState
+
+object CameraFlashAuto : CameraFlashState()
+object CameraFlashOff : CameraFlashState()
+object CameraFlashOn : CameraFlashState()
+
+class FlashManager {
+  private val modes = listOf(CameraFlashAuto, CameraFlashOn, CameraFlashOff)
+  private var current = 0
+
+  fun switch(): CameraFlashState {
+    current = (++current) % modes.size
+    return modes[current]
+  }
+
+  fun current(): CameraFlashState {
+    return modes[current]
   }
 }
